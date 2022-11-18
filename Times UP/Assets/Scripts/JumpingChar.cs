0b0497c312d335d78d08dea9063d8c1b65d995ff
Nixon Scherbarth <nixonbusinessmail@gmail.com>
2022-11-18 23:15:04 +0100

    0.04
    Added working Jump script (Single Jump when collision with ground and Spacebar pressed)

--- Times UP/Assets/Scripts/JumpingChar.cs
+++ Times UP/Assets/Scripts/JumpingChar.cs
@@ -2,31 +2,25 @@ using UnityEngine;
 
 public class JumpingChar : MonoBehaviour
 {
+    public bool isGrounded = false;
     [SerializeField] private float jumpPower = 5.0f;
 
     private Rigidbody2D _playerRigidbody;
     private void Start()
     {
-        _playerRigidbody = GetComponent<Rigidbody2D>();
-        if (_playerRigidbody == null)
+
+    }
+    void Update ()
+    {
+        Jump();
+    }
+    void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            Debug.LogError("Player is missing a Rigidbody2D component");
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }
     }
-    private void Update()
-    {
-
-        if (Input.GetButton("Jump") && IsGrounded())
-            Jump();
-    }
-    
-    private void Jump() => _playerRigidbody.velocity = new Vector2(0, jumpPower);
-
-    private bool IsGrounded()
-    {
-        var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
-        return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
-    }
-
 
 }
