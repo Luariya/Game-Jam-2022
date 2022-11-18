25ed579ddc0548f8735dbf55a4b384d7effb5c55
Luariya <nelewegner@gmx.de>
2022-11-18 20:50:53 +0100

    0.0.2
    movement script

--- Times UP/Assets/Scripts/Movement_Player.cs
+++ Times UP/Assets/Scripts/Movement_Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement2D : MonoBehaviour
+public class Movement_Player : MonoBehaviour
 {
     Rigidbody2D body;
 
