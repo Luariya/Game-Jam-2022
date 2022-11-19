5dd3574df9fdde2d2cffdb84ca1ea62c2f1a7ef9
Luariya <nelewegner@gmx.de>
2022-11-19 11:31:52 +0100

    0.0.7
    the right version

---
+++ Times UP/Assets/Scripts/CountDown.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CountDown : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] TMP_Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("00.0");
+    }
+}
