cfe84b79a55659b59460bafdd4f3723c9a7809de
Nixon Scherbarth <nixonbusinessmail@gmail.com>
2022-11-19 15:56:45 +0100

    Revert "0.1.9"
    This reverts commit f1c7fa5d64d0e71a50b42f2149a021a54c70002c.

--- Times UP/Assets/Scripts/ChangeSprite.cs
+++ Times UP/Assets/Scripts/ChangeSprite.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 
 public class ChangeSprite : MonoBehaviour
 {
-    public GameObject child, parent, grandparent;
-
-    public CountDown countDown;
+    public GameObject child;
+    public GameObject parent;
+    public GameObject grandparent;
 
     // Start is called before the first frame update
-
     void Start()
     {
         child.SetActive(true);
@@ -21,35 +20,20 @@ public class ChangeSprite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "ChildToAdult")
         {
             child.SetActive(false);
             parent.SetActive(true);
-            grandparent.SetActive(false);
         }
 
         if (other.tag == "AdultToGrandparent")
         {
-            child.SetActive(false);
             parent.SetActive(false);
             grandparent.SetActive(true);
         }
-        if (other.tag == "Powerup")
-        {
-            countDown.CurrentTime += 5f;
-            Nixon(other.gameObject);
-        }
-    }
-
-    private void Nixon(GameObject gO)
-    {
-        Destroy(gO);
-        //if else klausel in kurz geschrieben unten
-        //int f = (4 == 4) ? 5 : 7; 
-
     }
 }
